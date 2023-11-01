ca6e9db7a2066ee06cb4c45605ab52fbe685c77c
Ernazar Doronov <ernazardoronov@gmail.com>
2023-11-02 00:01:51 +0900

    init

---
+++ src/main/java/com/ernazar/movies/Review.java
@@ -0,0 +1,19 @@
+package com.ernazar.movies;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection="reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Review {
+    @Id
+    private Object id;
+    private String body;
+}
---
+++ src/test/java/com/ernazar/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ernazar.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
